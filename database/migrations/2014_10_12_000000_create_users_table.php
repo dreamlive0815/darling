dd3e0d819762aa6e1bc17e449f4519e794e0ad4e
DreamLive <995928339@qq.com>
2018-03-14 12:01:03 +0800

    201803141200

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,12 +19,16 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('nickname')->nullable();
+            $table->string('realname')->nullable();
             $table->string('sex', 3)->nullable();
             $table->integer('age')->nullable();
             $table->string('job')->nullable();
             $table->string('address')->nullable();
+            $table->string('company')->nullable();
             $table->string('description')->nullable();
             $table->integer('state')->nullable();
+            $table->boolean('is_corridor')->default(0);
+            $table->integer('credit_score')->default(50);
             $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
