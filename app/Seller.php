1d9904fe9811ab73c49f36b9c458fea194188604
unknown <>
2018-03-15 11:25:11 +0800

    201803151124

--- app/Seller.php
+++ app/Seller.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Seller extends Model
+class Seller extends Authenticatable
 {
-    //
+    use Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
