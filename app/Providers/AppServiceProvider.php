2b995e50dd7214bb9b577a71a0abc9cc008a831f
unknown <>
2018-03-20 14:38:30 +0800

    201803201438

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
 
@@ -14,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        DB::listen(function ($query) {
+            //echo $query->sql . "\n";
+        });
         Relation::morphMap([
             'user' => \App\User::class,
             'seller' => \App\Seller::class, 
